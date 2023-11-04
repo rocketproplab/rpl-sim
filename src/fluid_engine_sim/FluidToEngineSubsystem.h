61490eeabfebf6573bf2037f7b7377222212a4f8
Taanish Reja <tnreja@gmail.com>
2023-11-04 12:18:44 -0700

    added a vector for solenoids

--- src/fluid_engine_sim/FluidToEngineSubsystem.h
+++ src/fluid_engine_sim/FluidToEngineSubsystem.h
@@ -8,18 +8,22 @@ class FluidToEngineSubsystem {
 
         }
 
+        // thing for actuating solenoids; are we going to make a seperate type here for solenoids? Is there a solenoid library we're using?
+        void setSolenoidState(int index, bool isOpen) {
+            // TODO
+            solenoids[index] = isOpen;
+        }
 
 
     private:
+        // Honestly not sure how we're implementing this, but a vector makes sense to me here.
+        // If we got the vector route, we need to include the standard library vector. 
+        // I'm not doing it right now because I do not want to push a build task with the paths from my own system to the repo
+        std::vector<bool> solenoids;
+
         // simulates ignition firing
         void ignite() {
             // TODO
         }
-
-        // thing for actuating solenoids; are we going to make a seperate type here for solenoids? Is there a solenoid library we're using?
-        void setSolenoidState(int index, bool isOpen) {
-            // TODO
-        }
-
     
 };

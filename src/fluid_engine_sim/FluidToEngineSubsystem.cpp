e091754e7a095adf4897e16687b68991f980577e
Taanish Reja <tnreja@gmail.com>
2023-12-02 11:10:17 -0800

    finished process function (checking for bad inputs)

--- src/fluid_engine_sim/FluidToEngineSubsystem.cpp
+++ src/fluid_engine_sim/FluidToEngineSubsystem.cpp
@@ -6,6 +6,9 @@ class FluidToEngineSubsystem {
     public:
         FluidToEngineSubsystem ():status {WAITING_FOR_LAUNCH_COMMAND}
         {};
+
+        int LNG_solenoid = 0; // index for solenoid from Liquid Nitrogen tank to combustion chamber
+        int LOX_solenoid = 1; // index for solenoid from Liquid Oxygen tank to combustion chamber
         
         // called once per simulation step
         void process();
@@ -13,11 +16,8 @@ class FluidToEngineSubsystem {
         //     abort();
         // }
 
-        // set LNGSolenoid state
-        void setLNGSolenoid(bool isOpen);
-
-        // set LOXSolenoid state
-        void setLOXSolenoid(bool isOpen);
+        // set solenoid state
+        void setSolenoidState(int index, bool isOpen);
 
         // simulates ignition firing
         void ignite();
@@ -28,29 +28,41 @@ class FluidToEngineSubsystem {
 
     private:
         int status;
-        bool LNG_solenoid = false;; // solenoid from Liquid Nitrogen tank to combustion chamber
-        bool LOX_solenoid = false;; // solenoid from Liquid Oxygen tank to combustion chamber
-
+        std::vector<bool> solenoids = {false, false};
 };
 
 // process loop; if something goes wrong, catch error and crash program
 void FluidToEngineSubsystem::process() {
     try {
-        if (getStatus() == WAITING_FOR_LAUNCH_COMMAND) ignite();
-        // test conditionals that will blow up rocket
+        double solenoid_to_engine_run_timer = 0.0;
+        double target = 300; // placeholder value right now
+        if (getStatus() == WAITING_FOR_LAUNCH_COMMAND) {
+            setSolenoidState(LNG_solenoid, true);
+            setSolenoidState(LOX_solenoid, true);
+            setStatus(SOLENOIDS_OPEN_FUEL_FLOWING);
+        }
 
+        if (getStatus() == SOLENOIDS_OPEN_FUEL_FLOWING) {
+            if (solenoid_to_engine_run_timer > target) {
+                throw std::runtime_error("Window closed");
+            }
+        }
+
+        if (status == ENGINE_RUNNING) {
+            if (solenoids[LNG_solenoid] == false || solenoids[LOX_solenoid] == false) {
+                throw std::runtime_error("Attempted to close solenoid while engine running"); 
+            }
+        }
+
+        solenoid_to_engine_run_timer++;
     }catch(exception& e) {
         std::abort();
 
     }
 }
 
-void FluidToEngineSubsystem::setLNGSolenoid(bool isOpen) {
-    LNG_solenoid = isOpen;
-}
-
-void FluidToEngineSubsystem::setLOXSolenoid(bool isOpen) {
-    LOX_solenoid = isOpen;
+void FluidToEngineSubsystem::setSolenoidState(int index, bool isOpen) {
+    solenoids[index] = isOpen;
 }
 
 void FluidToEngineSubsystem::setStatus(int new_status) {
@@ -58,13 +70,13 @@ void FluidToEngineSubsystem::setStatus(int new_status) {
 }
 
 void FluidToEngineSubsystem::ignite() {
-    setLNGSolenoid(true);
-    setLOXSolenoid(true);
+    if (status != SOLENOIDS_OPEN_FUEL_FLOWING) {
+        throw std::runtime_error("Attempted ignition with solenoids closed"); 
+    }
 
-    setStatus(SOLENOIDS_OPEN_FUEL_FLOWING);
-
-    // wait 0.02-0.1 seconds (not sure if I should include this sleep step)
-    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    if (solenoids[LNG_solenoid] == false || solenoids[LOX_solenoid] == false) {
+        throw std::runtime_error("Attempted ignition with solenoids closed and status falsely set"); 
+    }
 
     setStatus(ENGINE_RUNNING);
 }
--- src/fluid_engine_sim/FluidToEngineSubsystem.h
+++ src/fluid_engine_sim/FluidToEngineSubsystem.h
@@ -2,33 +2,3 @@
 #include <exception>
 #include <thread>
 #include <chrono>
-
-enum FluidToEngineStatus {WAITING_FOR_LAUNCH_COMMAND, SOLENOIDS_OPEN_FUEL_FLOWING, ENGINE_RUNNING, BURNOUT};
-class FluidToEngineSubsystem {
-    public:
-        FluidToEngineStatus status;
-        
-        // called once per simulation step
-        void process();
-
-        // set LNGSolenoid state
-        void setLNGSolenoid(bool isOpen);
-
-        // set LOXSolenoid state
-        void setLOXSolenoid(bool isOpen);
-
-        // simulates ignition firing
-        void ignite();
-
-        /*
-            Returns the current status of this task (WAITING_FOR_LAUNCH_COMMAND, SOLENOIDS_OPEN_FUEL_FLOWING, ENGINE_RUNNING, BURNOUT)
-            If the status changed, this function should reflect the current state as of the most recent loop.
-        */
-        int getStatus();
-
-    private:
-        int status;
-        bool LNG_solenoid; // solenoid from Liquid Nitrogen tank to combustion chamber
-        bool LOX_solenoid; // solenoid from Liquid Oxygen tank to combustion chamber
-
-};

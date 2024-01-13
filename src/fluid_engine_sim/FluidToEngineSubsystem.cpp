e9edd5a7457c75028374d99323cb23cd18f6ef47
Taanish Reja <tnreja@gmail.com>
2024-01-13 11:11:48 -0800

    finished class, working on test cases

--- src/fluid_engine_sim/FluidToEngineSubsystem.cpp
+++ src/fluid_engine_sim/FluidToEngineSubsystem.cpp
@@ -1,86 +1,51 @@
 #include "FluidToEngineSubsystem.h"
 
-enum FluidToEngineStatus {WAITING_FOR_LAUNCH_COMMAND, SOLENOIDS_OPEN_FUEL_FLOWING, ENGINE_RUNNING, BURNOUT};
-
-class FluidToEngineSubsystem {
-    public:
-        FluidToEngineSubsystem ():status {WAITING_FOR_LAUNCH_COMMAND}
-        {};
-
-        int LNG_solenoid = 0; // index for solenoid from Liquid Nitrogen tank to combustion chamber
-        int LOX_solenoid = 1; // index for solenoid from Liquid Oxygen tank to combustion chamber
-        
-        // called once per simulation step
-        void process();
-        // if (true){ //solenoid in wrong state
-        //     abort();
-        // }
-
-        // set solenoid state
-        void setSolenoidState(int index, bool isOpen);
-
-        // simulates ignition firing
-        void ignite();
-
-        void setStatus(int new_status);
-
-        int getStatus();
-
-    private:
-        int status;
-        std::vector<bool> solenoids = {false, false};
-};
-
 // process loop; if something goes wrong, catch error and crash program
-void FluidToEngineSubsystem::process() {
+void FluidToEngineSubsystem::process(double solenoid_to_engine_run_timer, double target) {
     try {
-        double solenoid_to_engine_run_timer = 0.0;
-        double target = 300; // placeholder value right now
-        if (getStatus() == WAITING_FOR_LAUNCH_COMMAND) {
-            setSolenoidState(LNG_solenoid, true);
-            setSolenoidState(LOX_solenoid, true);
-            setStatus(SOLENOIDS_OPEN_FUEL_FLOWING);
+        if (getStatus() == FluidToEngineStatus::WAITING_FOR_LAUNCH_COMMAND) {
+            setSolenoidState(Solenoid::LNG_solenoid, true);
+            setSolenoidState(Solenoid::LOX_solenoid, true);
+            setStatus(FluidToEngineStatus::SOLENOIDS_OPEN_FUEL_FLOWING);
         }
 
-        if (getStatus() == SOLENOIDS_OPEN_FUEL_FLOWING) {
+        if (getStatus() == FluidToEngineStatus::SOLENOIDS_OPEN_FUEL_FLOWING) {
             if (solenoid_to_engine_run_timer > target) {
                 throw std::runtime_error("Window closed");
             }
         }
 
-        if (status == ENGINE_RUNNING) {
-            if (solenoids[LNG_solenoid] == false || solenoids[LOX_solenoid] == false) {
+        if (status == FluidToEngineStatus::ENGINE_RUNNING) {
+            if (solenoids[(int)Solenoid::LNG_solenoid] == false || solenoids[(int)Solenoid::LOX_solenoid] == false) {
                 throw std::runtime_error("Attempted to close solenoid while engine running"); 
             }
         }
-
-        solenoid_to_engine_run_timer++;
     }catch(exception& e) {
         std::abort();
 
     }
 }
 
-void FluidToEngineSubsystem::setSolenoidState(int index, bool isOpen) {
-    solenoids[index] = isOpen;
+void FluidToEngineSubsystem::setSolenoidState(Solenoid s, bool isOpen) {
+    solenoids[(int)s] = isOpen;
 }
 
-void FluidToEngineSubsystem::setStatus(int new_status) {
+void FluidToEngineSubsystem::setStatus(FluidToEngineStatus new_status) {
     status = new_status;
 }
 
 void FluidToEngineSubsystem::ignite() {
-    if (status != SOLENOIDS_OPEN_FUEL_FLOWING) {
+    if (status != FluidToEngineStatus::SOLENOIDS_OPEN_FUEL_FLOWING) {
         throw std::runtime_error("Attempted ignition with solenoids closed"); 
     }
 
-    if (solenoids[LNG_solenoid] == false || solenoids[LOX_solenoid] == false) {
+    if (solenoids[(int)Solenoid::LNG_solenoid] == false || solenoids[(int)Solenoid::LOX_solenoid] == false) {
         throw std::runtime_error("Attempted ignition with solenoids closed and status falsely set"); 
     }
 
-    setStatus(ENGINE_RUNNING);
+    setStatus(FluidToEngineStatus::ENGINE_RUNNING);
 }
 
-int FluidToEngineSubsystem::getStatus() {
+FluidToEngineStatus FluidToEngineSubsystem::getStatus() {
     return status;
 }
--- src/test.cpp
+++ src/test.cpp
@@ -1,10 +1,33 @@
-#include <iostream>
-#include "Eigen/Dense"
+#include "Catch2-devel/src/catch2/catch_test_macros.hpp"
+#include <FluidToEngineSubsystem.h>
 
-using namespace std;
-using Eigen::Vector3d;
-
-int main(){
-	Vector3d v(1,2,3);
-	cout << v << endl;
+// Checking to see default construtor sets status appropriately
+TEST_CASE("WAITING FOR LAUNCH COMMAND", "NORMAL") {
+	FluidToEngineSubsystem fs {};
+	REQUIRE( fs.getStatus() == FluidToEngineStatus::WAITING_FOR_LAUNCH_COMMAND)
 }
+
+// Status set incorrectly, solenoids weren't opened
+TEST_CASE("IGNITE", "Unexpected Status: WAITING FOR LAUNCH COMMAND") {
+	FluidToEngineSubsystem fs {};
+	REQUIRE_THROWS_AS(fs.ignite(), std::runtime_error);
+}
+
+// Status set correctly, solenoids weren't opened
+TEST_CASE("IGNITE", "CLOSED SOLENIDS") {
+	FluidToEngineSubsystem fs {};
+	fs.setStatus(FluidToEngineStatus::SOLENOIDS_OPEN_FUEL_FLOWING);
+	REQUIRE_THROWS_AS(fs.ignite(), std::runtime_error);
+}
+
+TEST_CASE("Process Loop Run", "Normal") {
+	FluidToEngineSubsystem fs {};
+	double solenoid_timer {0.0};
+	double target {300.0};
+	while (true) {
+		REQUIRE(fs.process(solenoid_timer, target), )
+		solenoid_timer++;
+	}
+}
+
+

d0e55ff4e7bf5d2a03d76d7e85ea1de7d3c934a2
Owen Bartolf <owen.bartolf@Gmail.com>
2024-04-02 13:15:22 -0700

    Remove embedded Boost

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
 # rpl-sim
+
+## Using `rpl-sim`
+
+### Error Codes
+
+- `0`: Success
+- `1`: Missing required command line argument.
+- `2`: Help command was received; displayed help instead of running.
+- `3`: A file had a parsing error. Check your JSON.
+
+- `3`: No serial port for flight computer provided, or did not explicitly use `--no-device` option to denote no explicit device.
+- `4`: Output File Path Invalid
+- `5`: Input File Could Not Be Opened
+
+## Developing `rpl-sim`
+
+
+
+### Getting Started (Unix Variants, Windows Subsystem for Linux)
+
+We recommend Windows users use Windows Subsystem for Linux (WSL).
+
+#### Install g++
+We use g++ as our primary compiler. Others should work, but using `g++` is proven to work.
+
+#### Install CMake
+We use CMake as a build system.
+
+#### Install Boost
+Boost is a large C++ library that `rpl-sim` uses for serial ports, ordinary differential equations, and more. Boost is not embedded inside of `rpl-sim` and must be downloaded separately.
+
+Follow the [instructions on the Boost website](https://www.boost.org/doc/libs/1_84_0/more/getting_started/unix-variants.html) to download the Boost library onto your machine.
+
+### Getting Started (Windows)
+
+#### Install Makefile for Windows
+
+
+### Building the Project
+Run `make` 
+
+### Code Style
+Follow [Google's C++ style guidelines](https://google.github.io/styleguide/cppguide.html).
--- src/main.cpp
+++ src/main.cpp
@@ -1,71 +1,19 @@
-#include "optparse.h"
+#include "exitCodeDefines.hpp"
+#include "SimParams.hpp"
 
 #include <iostream>
-
-const std::string version =
-    "%prog 1.0\nCopyright (C) 2023\n"
-    "TODO MIT LICENSE";
+#include <string>
+#include <boost/program_options.hpp>
 
 
+namespace po = boost::program_options;
 
-int main(int argc, char *argv[])
+int main(int ac, char *av[])
 {
+    SimParams p;
 
-    
-
-    optparse::OptionParserExcept parser = optparse::OptionParserExcept().version(version)
-          .description("Description")
-          .epilog("Epilog");
-
-
-    parser.add_option("--serial-port")
-        .help("The serial port at which the flight computer is connected.")
-        .type("string");
-
-    parser.add_option("--output")
-        .help("The path at which to write the simulation dump file.")
-        .type("string");
-
-    // --------------------------------
-
-    optparse::OptionGroup position_provider("Position Provider Data");
-
-    // TODO: Make these things matter.
-
-    position_provider.add_option("--pp-atmosphere-isa-csv")
-        .help("The path at which an atmosphere ISA datafile exists.")
-        .type("string");
-
-    position_provider.add_option("--pp-thrust-curve-csv")
-        .help("The path at which data for the thrust curve of the main engine exists.")
-        .type("string");
-
-    position_provider.add_option("--pp-mach-vs-cd.csv")
-        .help("TODO TODO IDK WTF THIS IS, ASK PROPULSION TEAM. REQUIRED THO :)")
-        .type("string");
-
-    parser.add_option_group(position_provider);
-
-    // --------------------------------
-
-    try
-    {
-        const optparse::Values &options = parser.parse_args(argc, argv);
-        const std::vector<std::string> args = parser.args();
-
-        std::cout << "output: " << options["output"] << std::endl;
-
-        std::cout << std::endl << "leftover arguments: " << std::endl;
-        for (std::vector<std::string>::const_iterator it = args.begin(); it != args.end(); ++it)
-        {
-            std::cout << "arg: " << *it << std::endl;
-        }
-    }
-    catch (int ret_code)
-    {
-        std::cout << "OptionParser has thrown " << ret_code << std::endl;
-        return ret_code;
-    }
-
-    return 0;
+    int exit_code = parse_command_line_arguments(p, ac, av);
+    return exit_code;
 }
+
+

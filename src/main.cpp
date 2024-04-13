73b9d17e367d7077d24f7bc47e23827a8b0e6526
Nathan Thomas <nathanthomas707@gmail.com>
2024-04-13 12:09:52 -0700

    Add output serde

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,18 @@
-
-#include "exitCodeDefines.hpp"
-
 #include "SimParams.hpp"
-
+#include "exitCodeDefines.hpp"
+#include "output_types.hpp"
 
 int main(int ac, char *av[])
 {
-
     ParseSimParamsResult result = get_params(ac, av);
+    SimParams params = result.sim_params;
+    DataLog data;
 
-    if (result.exit_code != EXIT_CODE_OK) { return result.exit_code; }
+    data.write_to_file(params.output_path);
+
+    if (result.exit_code != EXIT_CODE_OK) {
+        return result.exit_code;
+    }
 
     return EXIT_CODE_OK;
 }
-
-
